9100bd368ca1ee764ee807c57a4eb11a778641a3
hixcoder <hixcoder@gmail.com>
2023-10-11 10:26:22 +0100

    finish the node js course

--- nodejs/firstproject/index.js
+++ nodejs/firstproject/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
 const app = express();
+
+const Article = require("./models/Article");
+
+mongoose
+  .connect(
+    "mongodb+srv://hixcoder:12341234@hixdb.clv4wrq.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connect with db success");
+  })
+  .catch((error) => {
+    console.log("error" + error);
+  });
 
 // for accept body parameters
 app.use(express.json());
@@ -11,7 +25,16 @@ app.get("/", (req, res) => {
 
 // this is get request
 app.get("/hello", (req, res) => {
-  res.send("");
+  // for send text
+  // res.send("project path: " + __dirname + "views/hello.html");
+
+  // for send files
+  // res.sendFile(__dirname + "/views/hello.html");
+
+  // for send ejs
+  res.render("hello.ejs", {
+    name: "hixcoder",
+  });
 });
 
 // this is post request
@@ -30,7 +53,11 @@ app.get("/findSum/:n1/:n2", (req, res) => {
 // here we use body params
 app.get("/sayHello/", (req, res) => {
   console.log(req.body);
-  res.send(`hello to ${req.body.name}`);
+  // here we use json response to help the client parse data
+  res.json({
+    name: "robot",
+    talk: `hello ${req.body.name}`,
+  });
 });
 
 // here we use query params
@@ -39,6 +66,59 @@ app.get("/sayHi/", (req, res) => {
   res.send(`hi to ${req.query.name} ${req.query.nickname}`);
 });
 
+// here we use mongodb
+app.post("/articles", async (req, res) => {
+  const newArticle = new Article();
+  newArticle.title = req.body.title;
+  newArticle.body = req.body.body;
+  newArticle.numberOfLikes = req.body.numberOfLikes;
+  await newArticle.save();
+  res.send("the new article has been stored");
+});
+
+// get all articles
+app.get("/articles", async (req, res) => {
+  try {
+    const articles = await Article.find();
+    console.log(articles);
+    res.json(articles);
+  } catch (error) {
+    res.send("error: " + error);
+  }
+});
+
+// get specific article
+app.get("/articles/:articleId", async (req, res) => {
+  const id = req.params.articleId;
+  console.log("===> " + id);
+  try {
+    const article = await Article.findById(id);
+    res.json(article);
+  } catch (error) {
+    res.send("error: " + error);
+  }
+});
+
+// delete spesific article
+app.delete("/articles/:articleId", async (req, res) => {
+  const id = req.params.articleId;
+  console.log("===> " + id);
+  try {
+    const articles = await Article.findByIdAndDelete(id);
+    res.json(articles);
+  } catch (error) {
+    res.send("error: " + error);
+  }
+});
+
+// show articles
+app.get("/showArticles", async (req, res) => {
+  const articles = await Article.find();
+
+  res.render("articles.ejs", {
+    allArticles: articles,
+  });
+});
 app.listen(3000, () => {
   console.log("i am listening in port 3000");
 });
