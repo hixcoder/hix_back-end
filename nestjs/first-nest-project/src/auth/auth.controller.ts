3b81792c9c4b5916df3c1d38be11c408987b3dec
hixcoder <hixcoder@gmail.com>
2023-10-16 12:49:41 +0100

    finish setup of server and db and start auth

---
+++ nestjs/first-nest-project/src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Post, Req } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
+
+@Controller('auth')
+export class AuthController{
+    
+    constructor(private authService: AuthService){
+    }
+
+    @Post('signin')
+    signin(@Body() dto: AuthDto){
+        console.log({dto,})
+        return this.authService.signin()
+    }
+
+    @Post('signup')
+    signup(){
+        return this.authService.signup()
+    }
+}
