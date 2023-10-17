d588363f1186430cefa26e5ec8adc685872ac048
hixcoder <hixcoder@gmail.com>
2023-10-17 08:56:41 +0100

    trying to fix some db errors

--- nestjs/first-nest-project/src/auth/auth.service.ts
+++ nestjs/first-nest-project/src/auth/auth.service.ts
@@ -1,22 +1,30 @@
-import { Injectable } from "@nestjs/common";
-import { User, Bookmark } from "@prisma/client";
-import { PrismaService } from "src/prisma/prisma.service";
+import { Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
+import { AuthDto } from './dto';
+
 @Injectable({})
-export class AuthService{
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
 
-    constructor(private prisma: PrismaService){
+  signin() {
+    return {
+      msg: 'I m sign in',
+    };
+  }
 
-    }
-
-   signin(){
-        return {
-            msg: "I m sign in"
-        }
-    }
-
-    signup(){
-        return {
-            msg: "I m sign up"
-        }
-    }
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+    // save the new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+    // return the saved user
+    return user;
+  }
 }
--- nestjs/first-nest-project/src/prisma/prisma.service.ts
+++ nestjs/first-nest-project/src/prisma/prisma.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient{
-    constructor(){
-        super({
-            datasources: {
-                db: {
-                    url: "postgresql://postgres:123@localhost:5434/nest?schema=public"
-                }
-            }
-        })
-    }
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://postgres:123@localhost:5550/nest?schema=public?connect_timeout=300',
+        },
+      },
+    });
+  }
 }
