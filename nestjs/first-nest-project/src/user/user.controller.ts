f335911e6450165d7520bc288448e27f952f3cff
hixcoder <hixcoder@gmail.com>
2023-10-18 12:22:10 +0100

    finish the authentication

---
+++ nestjs/first-nest-project/src/user/user.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
+
+@Controller('users')
+export class UserController {
+  @UseGuards(JwtGuard)
+  @Get('me')
+  getMe(@Req() req: Request) {
+    return {
+      user: req.user,
+    };
+  }
+}
