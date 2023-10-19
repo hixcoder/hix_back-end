b2f0161bf41beab05a6d866b2735184cb6010ff8
hixcoder <hixcoder@gmail.com>
2023-10-19 11:15:19 +0100

    finish the nestjs course from freecodecamp

--- nestjs/first-nest-project/src/auth/auth.controller.ts
+++ nestjs/first-nest-project/src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
@@ -6,6 +13,7 @@ import { AuthDto } from './dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @HttpCode(HttpStatus.OK) // for make status code 200 not 201
   @Post('signin')
   signin(@Body() dto: AuthDto) {
     return this.authService.signin(dto);
