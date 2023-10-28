d38cc12cd45682968860204b5491b9a69941f582
hixcoder <hixcoder@gmail.com>
2023-10-28 13:07:32 +0100

    finish the prisma course

---
+++ nodejs/prisma project/prisma/migrations/20231028103216_/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userId` on the `UserPreference` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[UserPreferenceId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- DropForeignKey
+ALTER TABLE "UserPreference" DROP CONSTRAINT "UserPreference_userId_fkey";
+
+-- DropIndex
+DROP INDEX "UserPreference_userId_key";
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "UserPreferenceId" TEXT;
+
+-- AlterTable
+ALTER TABLE "UserPreference" DROP COLUMN "userId";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_UserPreferenceId_key" ON "User"("UserPreferenceId");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_UserPreferenceId_fkey" FOREIGN KEY ("UserPreferenceId") REFERENCES "UserPreference"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- nodejs/prisma project/script.ts
+++ nodejs/prisma project/script.ts
@@ -2,8 +2,37 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const users = await prisma.user.findMany();
-  console.log(users);
+  // await prisma.user.deleteMany();
+  // const users = await prisma.user.createMany({
+  //   data: [
+  //     {
+  //       email: "hixmsan@gmail.com",
+  //       name: "hix",
+  //       age: 44,
+  //     },
+  //     {
+  //       email: "hixman@gmail.com",
+  //       name: "hix",
+  //       age: 4,
+  //     },
+  //     {
+  //       email: "mano@gmail.com",
+  //       name: "hix",
+  //       age: 444,
+  //     },
+  //   ],
+  // });
+  const user = await prisma.user.updateMany({
+    where: {
+      name: {
+        startsWith: "hix",
+      },
+    },
+    data: {
+      name: "ddd@gmail.com",
+    },
+  });
+  console.log(user);
 }
 
 main()
